0f2f5059804bc552b2ede5bfe34e91ee0673b5fe
Sam Loeschen <samloeschen@gmail.com>
2020-09-01 13:46:33 -0500

    fish probability portraits, add probability sliders

--- Assets/Scripts/ProbabiilityFishPortrait.cs
+++ Assets/Scripts/ProbabiilityFishPortrait.cs
@@ -1,6 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ProbabiilityFishPortrait : MonoBehaviour {
+
+    [Range(0f, 1f)]
+    public float probability;
+    public Sprite mysterySprite;
+    public SpriteRenderer portraitSpriteRenderer;
+    public Sprite unlockSprite;
+    public Sprite lockSprite;
+    public SpriteRenderer lockStateSpriteRenderer;
+    public TextMeshPro probabilityTMPro;
+    public CharArray charArray;
+
+    public void SetFishDataObject(FishDataObject fishDataObject, GameState gameState, bool animate = true) {
+        if (gameState.unlockedFishNames.Contains(fishDataObject.data.name)) {
+            portraitSpriteRenderer.sprite = fishDataObject.data.profileSprite;
+            lockStateSpriteRenderer.sprite = unlockSprite;
+        } else {
+            portraitSpriteRenderer.sprite = mysterySprite;
+            lockStateSpriteRenderer.sprite = lockSprite;
+        }
+    }
+
+    void Awake() {
+        charArray = new CharArray(4);
+    }
+
+    void Update () {
+        // update text
+        int textProbability = (int)(probability * 100);
+        charArray.Clear();
+        if (textProbability < 10) {
+            charArray.Append(0);
+        }
+        charArray.Append(textProbability);
+        charArray.Add('%');
+
+        probabilityTMPro.SetCharArray(charArray.GetArray(), 0, charArray.count);
+    }
 }
---
+++ Assets/Shaders/PaletteColorUnlit.shader
@@ -0,0 +1,51 @@
+﻿Shader "Unlit/PaletteColorUnlit"
+{
+    Properties
+    {
+        _PaletteTexture ("Palette Texture", 2D) = "white" {}
+        _PaletteValue ("Palette Slider", Range(0,1)) = 0.5
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            uniform sampler2D _PaletteTexture;
+            uniform half _PaletteValue;
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                return tex2D(_PaletteTexture, float2(_PaletteValue, 0.5));
+            }
+            ENDCG
+        }
+    }
+}
